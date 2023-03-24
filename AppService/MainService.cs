ed58bc277f4e75cd5ed3a055fe5f89c8ac450aa2
Mohamed Magdy <magdy8912@gmail.com>
2023-03-25 00:38:28 +0200

    final register work without check if user register before

--- AppService/MainService.cs
+++ AppService/MainService.cs
@@ -1,9 +1,26 @@
-﻿namespace YallaHelp2023.AppService
+﻿using System.Reflection;
+
+namespace YallaHelp2023.AppService
 {
     public class MainService
     {
         public void NotifyStateChanged() => OnChange?.Invoke();
         public event Action? OnChange;
+		public bool IsAnyNullOrEmpty(object myObject)
+		{
+			foreach (PropertyInfo pi in myObject.GetType().GetProperties())
+			{
+				if (pi.PropertyType == typeof(string))
+				{
+					string value = (string)pi.GetValue(myObject);
+					if (string.IsNullOrEmpty(value))
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 
-    }
+	}
 }
