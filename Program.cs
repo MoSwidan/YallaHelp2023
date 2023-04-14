47a6c4bce712e59a7b827220e7e75e5bd22b7fa5
MoMagdy <MoMagdy@DESKTOP-39TPR25>
2023-04-14 14:25:18 +0200

    api live

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7187") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7187") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://yallahelp.getorders.net") });
 builder.Services.AddScoped<MainService>();
 builder.Services.AddScoped<AlertService>();
 builder.Services.AddSubtleCrypto(opt =>
