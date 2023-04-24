d1e9a3f775a4e52045f705ef1c460e96286c1f6f
Mohamed Magdy <magdy8912@gmail.com>
2023-04-24 16:33:31 +0200

    fix post responsive likes & comment in sep comp change app icons

--- Models/Like.cs
+++ Models/Like.cs
@@ -5,6 +5,6 @@
         public Guid Like_Id { get; set; }
         public Guid Post_Id { get; set; }
         public Guid User_Id { get; set; }
-        public DateTime? TimeStamp { get; set; }
+        public string? TimeStamp { get; set; }
     }
 }
