b40cf97d2d8a0a2e668b8ef39bb6b1abab04983d
Mohamed Magdy <magdy8912@gmail.com>
2023-04-25 01:43:16 +0200

    add likes

--- Models/Like.cs
+++ Models/Like.cs
@@ -4,7 +4,11 @@
     {
         public Guid Like_Id { get; set; }
         public Guid Post_Id { get; set; }
-        public Guid User_Id { get; set; }
+        public Guid? User_Id { get; set; }
         public string? TimeStamp { get; set; }
+    }
+    public class LikeResponse : BaseDto
+    {
+        public List<Like>? Data { get; set; }
     }
 }
