6d87e4c2d13b2e99617d5add01b5913107b709c1
Mohamed Magdy <magdy8912@gmail.com>
2023-04-26 23:02:47 +0200

    fav likes comments

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -4,8 +4,18 @@
     {
         public Guid Comment_Id { get; set; }
         public Guid Post_Id { get; set; }
-        public Guid User_Id { get; set; }
-        public string? CommentContent { get; set; }
+        public Guid? User_Id { get; set; }
+        public string? Comment_Content { get; set; }
         public string? TimeStamp { get; set; }
+    }
+    public class CommentView : Comment
+    {
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Image_Url { get; set; }
+    }
+    public class CommentResponse : BaseDto
+    {
+        public List<CommentView>? Data { get; set; }
     }
 }
