f03dffd6c96eb8dae3eaad46326395a9a79cf843
Mohamed Magdy <magdy8912@gmail.com>
2023-04-27 03:35:39 +0200

    enhace

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -3,7 +3,7 @@
     public class Comment
     {
         public Guid Comment_Id { get; set; }
-        public Guid Post_Id { get; set; }
+        public Guid? Post_Id { get; set; }
         public Guid? User_Id { get; set; }
         public string? Comment_Content { get; set; }
         public string? TimeStamp { get; set; }
