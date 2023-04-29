80076510ad2e1dd1c2c847390dde342990979392
Mohamed Magdy <magdy8912@gmail.com>
2023-04-29 05:21:32 +0300

    notifaction

---
+++ Models/NotificationModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace YallaHelp2023.Models
+{
+    public class NotificationModel
+    {
+        [JsonProperty("deviceId")]
+        public string DeviceId { get; set; }
+        [JsonProperty("isAndroiodDevice")]
+        public bool IsAndroiodDevice { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+    public class ResponseModel
+    {
+        [JsonProperty("isSuccess")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
